a60455136f0838ba3d8bf114f7e752a258086afa
unknown <amarjeet@baruzotech.com>
2023-03-28 10:46:14 +0530

    add customer

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,5 +1,5 @@
-import { instance } from "../middleware/razorpay.middleware.js";
-import { Customer } from "../model/customer.model.js";
+import { instance } from '../middleware/razorpay.middleware.js';
+import { Customer } from '../model/customer.model.js';
 
 export const createCustomer = async (req, res) => {
   try {
@@ -23,4 +23,79 @@ export const createCustomer = async (req, res) => {
       .status(Number(`${error.statusCode}`))
       .send({ status: false, message: error });
   }
+};
+
+export const editCustomer = async (req, res) => {
+  const id = req.params.id;
+  const { name, contact, email } = req.body;
+  instance.customers.edit(
+    id,
+    {
+      name: name,
+      email: email,
+      contact: contact,
+    },
+    async (err, data) => {
+      if (err) {
+        return res.status(400).send({ status: false, message: err });
+      }
+      await Customer.findOneAndUpdate(
+        { id: id },
+        {
+          name: name,
+          email: email,
+          contact: contact,
+        }
+      );
+      const sendData = {
+        name,
+        email,
+        contact,
+      };
+      return res
+        .status(200)
+        .send({ status: true, message: 'customer updated', sendData });
+    }
+  );
+};
+
+export const allCustomer = async (req, res) => {
+  try {
+    instance.customers.all({}, (err, data) => {
+      if (err) {
+        return res.status(400).send({ status: true, message: err });
+      }
+      const result = data.items.map((customer) => {
+        const customerData = {
+          name: customer.name,
+          email: customer.email,
+          contact: customer.contact,
+        };
+        return customerData;
+      });
+      data.items = result;
+      return res
+        .status(200)
+        .json({ status: false, message: 'all customers', data });
+    });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error });
+  }
+};
+
+export const customerData = async (req, res) => {
+  const id = req.params.id;
+  instance.customers.fetch(id, (err, data) => {
+    if (err) {
+      return res.status(400).send({ status: false, message: err });
+    }
+    const customerData = {
+      name: data.name,
+      email: data.email,
+      contact: data.contact,
+    };
+    return res
+      .status(200)
+      .send({ status: true, message: 'customer data', customerData });
+  });
 };
--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,8 +1,16 @@
 import express from 'express';
-import { createCustomer } from '../controllers/customerController.js';
+import {
+  allCustomer,
+  createCustomer,
+  customerData,
+  editCustomer,
+} from '../controllers/customerController.js';
 
 const customerRouter = express.Router();
 
+customerRouter.get('/', allCustomer);
+customerRouter.get('/:id', customerData);
 customerRouter.post('/', createCustomer);
+customerRouter.patch('/:id', editCustomer);
 
 export default customerRouter;
